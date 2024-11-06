96e8fb7f5ac35d79f7eed853583d5297546b2184
brina03 <shabrinamputri@gmail.com>
2024-11-06 12:58:37 +0700

    Add files via upload
    toko roti

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Toggle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika Humberger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Klik di luar side bar ubtuk menghilangkan  nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+// Slider img
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length)
+  {
+    slideIndex = 1;
+  }
+  if (n < 1)
+  {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++)
+  {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
